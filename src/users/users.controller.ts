b25416c18e320e351aa0098ba603c94f177af4a5
HyeyeongLee <hylee@entcrowd.com>
2022-08-30 15:46:18 +0900

    add singup

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,18 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
 
-@Controller('users')
-export class UsersController {}
+@Controller('/users')
+export class UsersController {
+  // 회원가입
+  @Get()
+  async test(): Promise<string> {
+    return 'hello world';
+  }
+
+  // ts : void는 결과 값을 반환하지 않는 함수에 설정
+  @Post()
+  async createUser(@Body() dto: CreateUserDto): Promise<any> {
+    console.log(dto);
+    return dto;
+  }
+}
