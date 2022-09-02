b116bc9a2fa912a2cab1f22c8b7d90b718ebfae9
HyeyeongLee <hylee@entcrowd.com>
2022-09-02 11:42:27 +0900

    first commit to gitlab

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,39 +1,72 @@
-import { Body, Controller, Get, Param, Post, Query } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  HttpException,
+  Param,
+  ParseIntPipe,
+  Post,
+  Query,
+} from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { VerifyEmailDto } from './dto/verify-email.dto';
 import { UserLoginDto } from './dto/user-login.dto';
 import { UsersService } from './users.service';
+import { ValidationPipe } from '../validation.pipe';
 // import { UserInfo } from './UserInfo';
 
-@Controller('/users')
+@Controller()
 export class UsersController {
   constructor(private UsersService: UsersService) {}
-  // 회원가입
-  // ts : void는 결과 값을 반환하지 않는 함수에 설정
-  @Post()
-  async createUser(@Body() dto: CreateUserDto): Promise<void> {
-    const { name, email, password } = dto;
-    await this.UsersService.createUser(name, email, password);
+  @Get()
+  async test(): Promise<string> {
+    throw new HttpException(
+      {
+        statusCode: 404,
+        message: '에러발생!! 404',
+      },
+      403,
+    );
   }
 
-  //   이메일 검증
-  @Post('/email-verify')
-  async verifyEmail(@Query() dto: VerifyEmailDto): Promise<string> {
-    console.log(dto);
-    return;
+  @Get(':id')
+  async bingPipes(@Param('id', ParseIntPipe) id: number): Promise<string> {
+    return `id ${id}`;
   }
 
-  //   로그인
-  @Post('/login')
-  async login(@Body() dto: UserLoginDto): Promise<string> {
-    console.log(dto);
-    return;
-  }
+  //   custom pipes
+  //   @Get(':id')
+  //   async bingPipes(@Param('id', ValidationPipe) id: number): Promise<string> {
+  //     return `id ${id}`;
+  //   }
 
-  //   고객 정보 가져오기
-  @Get('/:id')
-  async getUserInfo(@Param('id') userId: string): Promise<any> {
-    console.log(userId);
-    return;
-  }
+  //   // 회원가입
+  //   // ts : void는 결과 값을 반환하지 않는 함수에 설정
+  //   @Post()
+  //   async createUser(@Body() dto: CreateUserDto): Promise<CreateUserDto> {
+  //     const { name, email, password } = dto;
+  //     return dto;
+  //     // await this.UsersService.createUser(name, email, password);
+  //   }
+
+  //   //   이메일 검증
+  //   @Post('/email-verify')
+  //   async verifyEmail(@Query() dto: VerifyEmailDto): Promise<string> {
+  //     console.log(dto);
+  //     return;
+  //   }
+
+  //   //   로그인
+  //   @Post('/login')
+  //   async login(@Body() dto: UserLoginDto): Promise<string> {
+  //     console.log(dto);
+  //     return;
+  //   }
+
+  //   //   고객 정보 가져오기
+  //   @Get('/:id')
+  //   async getUserInfo(@Param('id') userId: string): Promise<any> {
+  //     console.log(userId);
+  //     return;
+  //   }
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,5 +1,6 @@
 import * as uuid from 'uuid';
 import { Injectable } from '@nestjs/common';
+import { EmailService } from 'src/email/email.service';
 
 // UUID = Universally Unique IDentifier
 // v1 : 타임스탬프(시간) 기준으로 UUID 생성
@@ -7,31 +8,31 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class UsersService {
   async createUser(name: string, email: string, password: string) {
-    await this.checkUserExists(email);
+    // await this.checkUserExists(email);
 
     const signupVerifyToken = uuid.v1();
 
-    await this.saveUser(name, email, password, signupVerifyToken);
-    await this.sendMemberJoinEmail(email, signupVerifyToken);
+    // await this.saveUser(name, email, password, signupVerifyToken);
+    // await this.sendMemberJoinEmail(email, signupVerifyToken);
   }
 
-  private checkUserExists(email: string) {
-    return false; //db 연동 후 구현
-  }
+  //   private checkUserExists(email: string) {
+  //     return false; //db 연동 후 구현
+  //   }
 
-  private saveUser(
-    name: string,
-    email: string,
-    password: string,
-    signupVerifyToken,
-  ) {
-    return; //db 연동 후 구현
-  }
+  //   private saveUser(
+  //     name: string,
+  //     email: string,
+  //     password: string,
+  //     signupVerifyToken,
+  //   ) {
+  //     return; //db 연동 후 구현
+  //   }
 
-  private async sendMemberJoinEmail(email: string, signupVerifyToken: string) {
-    await this.emailService.sendMemberJoinVerification(
-      email,
-      signupVerifyToken,
-    );
-  }
+  //   private async sendMemberJoinEmail(email: string, signupVerifyToken: string) {
+  //     await this.emailService.sendMemberJoinVerification(
+  //       email,
+  //       signupVerifyToken,
+  //     );
+  //   }
 }
