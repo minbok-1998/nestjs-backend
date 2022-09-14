d4eaffbc708e019b8a960fa60bbfe0ac10044093
HyeyeongLee <hylee@entcrowd.com>
2022-09-14 17:10:46 +0900

    메인페이지와 회원가입 페이지 이동을 위해 연결

--- server/src/signup/signup.controller.ts
+++ server/src/signup/signup.controller.ts
@@ -1,10 +1,17 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post, Render } from '@nestjs/common';
 import { SignupService } from './signup.service';
 import { SignUpDto } from './dto/signup.dto';
 
 @Controller('/signup')
 export class SignupController {
   constructor(private SignupService: SignupService) {}
+
+  // 고객 목록 불러오기
+  @Get('/')
+  @Render('/pages/signup')
+  async signupView() {
+    return {};
+  }
 
   @Post()
   async signup(@Body() signupData: SignUpDto): Promise<SignUpDto> {
