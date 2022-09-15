4318b22fe15b95a3f6766f41600a0c8d20a09385
HyeyeongLee <hylee@entcrowd.com>
2022-09-15 11:21:53 +0900

    add login client

--- server/src/signup/signup.controller.ts
+++ server/src/signup/signup.controller.ts
@@ -2,18 +2,25 @@ import { Body, Controller, Get, Post, Render } from '@nestjs/common';
 import { SignupService } from './signup.service';
 import { SignUpDto } from './dto/signup.dto';
 
-@Controller('/signup')
+@Controller('/')
 export class SignupController {
   constructor(private SignupService: SignupService) {}
 
-  // 고객 목록 불러오기
-  @Get('/')
+  // 회원가입 창 불러오기
+  @Get('/signup')
   @Render('/pages/signup')
   async signupView() {
     return {};
   }
 
-  @Post()
+  // 로그인 창 불러오기
+  @Get('/login')
+  @Render('/pages/login')
+  async loginView() {
+    return {};
+  }
+
+  @Post('/signup')
   async signup(@Body() signupData: SignUpDto): Promise<SignUpDto> {
     const result = await this.SignupService.signUp(signupData);
     return result;
